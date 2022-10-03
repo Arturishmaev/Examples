cc55738a268212dc3e1f33d7b92795037188e91b
ArturIshmaev <arturishmaev@yandex.ru>
2022-10-04 01:04:34 +0500

    last commit

--- Examples013_RecursionAlgorithm/Program.cs
+++ Examples013_RecursionAlgorithm/Program.cs
@@ -1,1 +1,76 @@
-﻿
+﻿string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+
+}
+
+
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
+
+int Factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+System.Console.WriteLine(Factorial(5));
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(double n)
+{
+    if(n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+for (int i = 1; i < 10; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
